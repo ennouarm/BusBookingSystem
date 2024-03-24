eaa20ad056fa152e00a8704a5e4cb472ce8924d9
ennouarm <ennm05@uqo.ca>
2024-03-23 21:15:07 -0400

    First commit.Bus Management System : Case Number 4.

---
+++ src/bus/booking/BusManagement.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package bus.booking;
+
+/**
+ *
+ * @author HP 650
+ */
+public class BusManagement {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       MainScreen mr = new MainScreen();
+       mr.setLocationRelativeTo(null);
+       mr.setVisible(true);
+       
+    }
+    
+}
